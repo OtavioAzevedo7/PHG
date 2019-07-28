7020e4d4cb00a70bfb640f264f3c872dbc6babff
Otávio Azevedo <otavioazevedo7@gmail.com>
2019-07-28 19:25:49 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,29 @@ namespace SalesWebMvc.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
+		//Cada departamento tem uma List de vendedores
+		public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+		//Construtor sem argumentos é necessário quando há outro construtor com argumentos;
+		public Department()
+		{
+		}
+
+		public Department(int id, string name)
+		{
+			Id = id;
+			Name = name;
+		}
+
+		public void AddSeller(Seller seller)
+		{
+			Sellers.Add(seller);
+		}
+
+		public double TotalSales (DateTime initial, DateTime final)
+		{
+			//Para cada vendedor da lista, chama o TotalSales do vendedor no período e soma o total de todos vendedores do departamento
+			return Sellers.Sum(seller => seller.TotalSales(initial, final));
+		}
 	}
 }
---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+	public class SalesRecord
+	{
+		public int Id { get; set; }
+		public DateTime Date { get; set; }
+		public double Amount { get; set; }
+		public SalesStatus Status { get; set; }
+		//Cada vendedor tem um SalesRecord
+		public Seller Seller { get; set; }
+
+		//Construtor sem argumentos é necessário quando há outro construtor com argumentos;
+		public SalesRecord()
+		{
+		}
+
+		public SalesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
+		{
+			Id = id;
+			Date = date;
+			Amount = amount;
+			Status = status;
+			Seller = seller;
+		}
+	}
+}
